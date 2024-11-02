307c1db72a3c5f733e58aa50c9a7b6269c6c7f36
MadalalakshmiManasa <110586241+MadalalakshmiManasa@users.noreply.github.com>
2024-11-02 16:10:18 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+let age = 22; 
+var surname="Madala"                   
+const name = "ManasaSai";         
+
+console.log("Age:", age);  
+console.log("Constant Name:", name); 
+console.log(surname);
